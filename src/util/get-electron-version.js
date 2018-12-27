f7afb333dd2b39f8c03cc15a43696dafbd52fcbf
Mark Lee <electronjs@lazymalevolence.com>
2018-12-27 14:38:05 -0800

    Move the thrown exception to before rebuild is called

--- src/util/get-electron-version.js
+++ src/util/get-electron-version.js
@@ -1,5 +1,4 @@
 import debug from 'debug';
-import fs from 'fs-extra';
 import path from 'path';
 import readPackageJSON from './read-package-json';
 
@@ -8,14 +7,9 @@ const d = debug('electron-forge:util');
 export default async (projectDir) => {
   let result = null;
 
-  const baseModulesDir = path.join(projectDir, 'node_modules');
-  if (!await fs.pathExists(baseModulesDir)) {
-    throw new Error("Could not find the node_modules folder. Please run 'npm install' and try again.");
-  }
-
   const modulesToExamine = ['electron-prebuilt-compile', 'electron', 'electron-prebuilt'];
   for (const moduleName of modulesToExamine) {
-    const moduleDir = path.join(baseModulesDir, moduleName);
+    const moduleDir = path.join(projectDir, 'node_modules', moduleName);
     try {
       const packageJSON = await readPackageJSON(moduleDir);
       result = packageJSON.version;
